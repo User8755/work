b4fb53e9cb05b3797ee021a7fbc7fd814c5354db
Nata Ponamarenko <nuts08air@inbox.ru>
2023-11-16 14:21:41 +0800

    fix merge

--- src/CurrentTasks/CurrentTasks.jsx
+++ src/CurrentTasks/CurrentTasks.jsx
@@ -14,70 +14,13 @@ function CurrentTasks({
 }) {
   const handlerOpenModal = () => {
     setModal(true);
-    setChild(<NewTask isTasks={isTasks} setTasks={setTasks} setModal={setModal} />);
+    setChild(
+      <NewTask isTasks={isTasks} setTasks={setTasks} setModal={setModal} />
+    );
   };
 
   return (
     <div className='current-tasks'>
-      <button className='button' onClick={handlerOpenModal}>
-        Новая задача
-      </button>
-      <h2>Приоритет</h2>
-      <div className='current-tasks__continer'>
-        <div className='current-tasks__hi'>
-          <h2>Высокий</h2>
-          {isTasks.map((item) => {
-            if (item.priorities === 'Высокий' && !item.inprogress) {
-              return (
-                <Card
-                  key={item.task}
-                  tasks={item}
-                  setModal={setModal}
-                  setChild={setChild}
-                  setSelectedCard={setSelectedCard}
-                  selectedCard={selectedCard}
-                />
-              );
-            }
-          })}
-        </div>
-        <div className='current-tasks__mid'>
-          <h2>Средний</h2>
-          {isTasks.map((item) => {
-            if (item.priorities === 'Средний' && !item.inprogress) {
-              return (
-                <Card
-                  key={item.task}
-                  tasks={item}
-                  setModal={setModal}
-                  setChild={setChild}
-                  setSelectedCard={setSelectedCard}
-                  selectedCard={selectedCard}
-                ></Card>
-              );
-            }
-          })}
-        </div>
-        <div className='current-tasks__low'>
-          <h2>Низкий</h2>
-          {isTasks.map((item) => {
-            if (
-              item.priorities === 'Низкий' &&
-              !item.inprogress &&
-              !item.completed
-            ) {
-              return (
-                <Card
-                  key={item.task}
-                  tasks={item}
-                  setModal={setModal}
-                  setChild={setChild}
-                  setSelectedCard={setSelectedCard}
-                  selectedCard={selectedCard}
-                ></Card>
-              );
-            }
-          })}
       <div className='main-wrapper'>
         <aside className='aside'>
           <ul className='aside__list'>
@@ -88,22 +31,14 @@ function CurrentTasks({
           <button className='button' onClick={handlerOpenModal}>
             Новая задача
           </button>
-          <p className='norm'>TEST test</p>
-          <p className='med'>TEST test</p>
-          <p className='smbold'>TEST test</p>
-          <p className='bold'>TEST test</p>
         </aside>
         <div className='content'>
           <h2 className='content__title'>Приоритет</h2>
           {/*<div className='current-tasks__continer'>*/}
           <div className='current-tasks__hi task'>
             <h2 className='content__subtitle'>Высокий</h2>
-            {newTasks.map((item) => {
-              if (
-                item.priorities === 'Высокий' &&
-                !item.inprogress &&
-                !item.completed
-              ) {
+            {isTasks.map((item) => {
+              if (item.priorities === 'Высокий' && !item.inprogress) {
                 return (
                   <Card
                     key={item.task}
@@ -117,14 +52,11 @@ function CurrentTasks({
               }
             })}
           </div>
+
           <div className='current-tasks__mid task'>
             <h2 className='content__subtitle'>Средний</h2>
-            {newTasks.map((item) => {
-              if (
-                item.priorities === 'Средний' &&
-                !item.inprogress &&
-                !item.completed
-              ) {
+            {isTasks.map((item) => {
+              if (item.priorities === 'Средний' && !item.inprogress) {
                 return (
                   <Card
                     key={item.task}
@@ -138,9 +70,10 @@ function CurrentTasks({
               }
             })}
           </div>
+
           <div className='current-tasks__low task'>
             <h2 className='content__subtitle'>Низкий</h2>
-            {newTasks.map((item) => {
+            {isTasks.map((item) => {
               if (
                 item.priorities === 'Низкий' &&
                 !item.inprogress &&
